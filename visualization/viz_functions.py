fba6f6774ad642a18bfd803aa4f6c613aeef6f56
André Patrício <andremppatricio@gmail.com>
2023-11-14 18:49:50 +0000

    Update visualization notebooks [skip ci]

--- visualization/viz_functions.py
+++ visualization/viz_functions.py
@@ -5,7 +5,9 @@ import math
 
 coolwarm_palette = sns.color_palette("coolwarm", 20)
 
-def barplot_ngrams(spark, query, x, y):
+def barplot_ngrams(spark, query, x, y, title=None):
+    if title is None:
+        title = "This week's most used keywords"
     results = spark.sql(query)
     results_df = results.toPandas()
 
@@ -17,7 +19,7 @@ def barplot_ngrams(spark, query, x, y):
     fig, ax = plt.subplots(1,1, figsize=(10,3))
     sns.barplot(x=x, y=y, hue=x, data=results_df, ax=ax, palette=color_values) # hue='word' for colored bars
     ax.invert_xaxis()
-    plt.title("This week's most used keywords")
+    plt.title(title)
     plt.xlabel("Keywords")
     plt.ylabel("Counts")
     plt.xticks(rotation=-45, ha='left') 
@@ -37,19 +39,23 @@ def wordcloud_ngrams(spark_session, query):
     plt.show()
 
 
-def lineplot_ngrams(spark_session, query, x, y, hue):
+def lineplot_ngrams(spark_session, query, x, y, hue, title=None):
+    if title is None:
+        title = "Evolution of keywords"
     results = spark_session.sql(query)
     results_df = results.toPandas()
-    fig, ax = plt.subplots(1,1, figsize=(10,3))
+    fig, ax = plt.subplots(1,1, figsize=(16,6))
     sns.lineplot(x=x, y=y, hue=hue, data=results_df, ax=ax) 
     # plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=len(results_df[hue]))  # Adjust the parameters as needed
     plt.legend(loc='upper left', bbox_to_anchor=(1, 1), ncol=math.ceil(len(results_df[hue].unique()) / 10))  # Adjust the parameters as needed
-    plt.title("Evolution of keywords")
+    plt.title(title)
     plt.xlabel("Week")
     plt.ylabel("Percentage per Week")
     plt.show()
 
-def barplot_journals(spark, query, x, y):
+def barplot_journals(spark, query, x, y, title=None):
+    if title is None:
+        title = "Journals with most publications"
     results_df = spark.sql(query).toPandas()
     values = results_df[y]
     normalized_values = (values - min(values)) / (max(values) - min(values))
@@ -58,15 +64,17 @@ def barplot_journals(spark, query, x, y):
 
     fig, ax = plt.subplots(1,1, figsize=(14,6), tight_layout=True)
     sns.barplot(x=x, y=y, hue=x, data=results_df, ax=ax, palette=color_values)
-    plt.title("This week's most used keywords")
+    plt.title(title)
     ax.invert_xaxis()
-    plt.xlabel("Keywords")
+    plt.xlabel("Journals")
     plt.ylabel("Counts")
     plt.xticks(rotation=-45, ha='left') 
     plt.show()
 
 
-def barplot_authors(spark, query, x, y):
+def barplot_authors(spark, query, x, y, title = None):
+    if title is None:
+        title = "Authors with most publications"
     results_df = spark.sql(query).toPandas()
     values = results_df[y]
     normalized_values = (values - min(values)) / (max(values) - min(values))
@@ -76,8 +84,8 @@ def barplot_authors(spark, query, x, y):
     fig, ax = plt.subplots(1,1, figsize=(14,6), tight_layout=True)
     sns.barplot(x=x, y=y, hue=x, data=results_df, ax=ax, palette=color_values) 
     ax.invert_xaxis()
-    plt.title("This week's most used keywords")
-    plt.xlabel("Keywords")
+    plt.title(title)
+    plt.xlabel("Authors")
     plt.ylabel("Counts")
     plt.xticks(rotation=-45, ha='left') 
     plt.show()
